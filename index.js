19de0c9befb50bc1472a82082cb51c5638c577c8
steph_sce <stephane@code-forefront.com>
2017-08-27 19:38:49 +0200

    refact index.js

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-function CalledOnLoad() {
-    function RandomQuote() {
-        var quotes = new Array();
-        quotes[0] = "JavaScript"
-        quotes[1] = "Angular"
-        quotes[2] = "ReactNative"
-        quotes[3] = "Vue.js"
-        quotes[4] = "Node.js"
-        return quotes[Math.floor(Math.random() * 4.99)];
-    }
-    function UpdateText() {
+var quotes = ["JavaScript", "Angular", "ReactNative","Vue.js", "Node.js"];
+
+window.onload = function () {
+    window.setInterval(function () {
         var text = document.getElementById("ShowText");
-        text.innerHTML = RandomQuote();
-    }
+        text.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+    }, 3000);
+};
 
-    window.setInterval(UpdateText, 3000);
-}
-
-window.onload = CalledOnLoad;
-
-
-//scroll
-function Scroll() {
-    (function ($) {
-        var elem = $.jInvertScroll(['.scroll'],
-            {
-                height: 6000,
-                onScroll: function (percent) {
-                    console.log(percent);
-                }
-            });
-
-        $(window).resize(function () {
-            if ($(window).width() <= 768) {
-                elem.destroy();
-            }
-            else {
-                elem.reinitialize();
-            }
-        });
-    }(jQuery));
-}
