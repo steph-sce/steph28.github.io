8c45d911f4d912b9af222b9727da7c30f9900f8f
steph28 <stephane.soucheyre@gmail.com>
2018-01-15 11:50:55 +0100

    ADD: scrollMagic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Debut texte aleatoire page accueil
-var quotes = ["JavaScript", "Angular","Vue.js", "Node.js", "HTML5/CSS3", "WEB & MOBILE"];
+var quotes = ["JavaScript", "Angular", "Vue.js", "Node.js", "HTML", "Mobile", "CSS"];
 
 window.onload = function () {
     window.setInterval(function () {
@@ -7,4 +7,47 @@ window.onload = function () {
         text.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
     }, 3000);
 };
-// Fin de changement de texte
+// Fin texte aleatoire
+
+// Animation Scroll
+/*$(function () { // wait for document ready
+    // init
+    var controller = new ScrollMagic.Controller();
+
+    // define movement of panels
+    var wipeAnimation = new TimelineMax()
+        // animate to second panel
+        .to("#slideContainer", 0.5, { z: -150 })		// move back in 3D space
+        .to("#slideContainer", 1, { x: "-25%" })	// move in to first panel
+        .to("#slideContainer", 0.5, { z: 0 })				// move back to origin in 3D space
+        // animate to third panel
+        .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+        .to("#slideContainer", 1, { x: "-50%" })
+        .to("#slideContainer", 0.5, { z: 0 })
+        // animate to forth panel
+        .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+        .to("#slideContainer", 1, { x: "-75%" })
+        .to("#slideContainer", 0.5, { z: 0 });
+
+    // create scene to pin and link animation
+    new ScrollMagic.Scene({
+        triggerElement: "#pinContainer",
+        triggerHook: "onLeave",
+        duration: "500%"
+    })
+        .setPin("#pinContainer")
+        .setTween(wipeAnimation)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+});*/
+// Fin Animation Scroll
+var controller = new ScrollMagic.Controller({ vertical: false });
+
+// build tween
+var tween = TweenMax.to("#target", 0.5, { backgroundColor: "green", width: "+=400" });
+
+// build scene
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger", duration: 500 })
+    .setTween(tween)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
